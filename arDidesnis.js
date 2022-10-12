16890aac5fb2d803c7053e7f1e8beae59cfb7411
Vytautas87 <vytautaszen1@gmail.com>
2022-10-12 11:02:51 +0300

    if

---
+++ arDidesnis.js
@@ -0,0 +1,25 @@
+let skaiciusx = +prompt("Įveskite x")
+let skaiciusy = +prompt("Įveskite y")
+let ardidesnisarmazesnisarlygus = ardidesnis(skaiciusx, skaiciusy)
+arskaiciusxdidesnisuzy(ardidesnisarmazesnisarlygus, skaiciusx, skaiciusy)
+
+
+function ardidesnis(x, y) {
+    if (x > y) {
+        return -1;
+    } else if (x < y) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+function arskaiciusxdidesnisuzy(didesnisArNe, skaiciusx, skaiciusy) {
+    if (didesnisArNe == -1) {
+        console.log(skaiciusx+ " didesnis uz " +skaiciusy);
+    } else if (didesnisArNe == 1) {
+        console.log(skaiciusx+ " mazesnis uz " +skaiciusy);
+    } else (didesnisArNe == 0) 
+        console.log(skaiciusx+ " lygus " +skaiciusy);
+}

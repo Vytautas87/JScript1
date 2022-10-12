d2b8b4851ea3e26cb3a43057f0ca0bf9b9bf80e9
Vytautas87 <vytautaszen1@gmail.com>
2022-10-12 11:32:05 +0300

    if

--- arstatusis.js
+++ arstatusis.js
@@ -1,20 +1,20 @@
 let krastinea = +prompt("Įveskite trikampio krastine a");
 let krastineb = +prompt("Įveskite trikampio krastine b");
 let krastinec = +prompt("Įveskite trikampio krastine c");
-let artrikampisstatus = arstatus(krastinea, krastineb, krastinec);
-artristatus(artrikampisstatus, krastinea, krastineb, krastinec);
+let arTrikampisStatus = arStatus(krastinea, krastineb, krastinec);
+arTriStatus(arTrikampisStatus);
 
 
-function arstatus(x, y, z) {
-    if (z ** 2 == x ** 2 + y ** 2) {
+function arStatus(x, y, z) {
+    if ((z ** 2) == (x ** 2) + (y ** 2)) {
         return true;
     } else {
         return false;
     }
 }
 
-function artristatus(Ar) {
-    if (Ar == true) {
+function arTriStatus(ar) {
+    if (ar == true) {
         console.log("Trikampis yra status");
     } else {
         console.log("Trikampis nėra status");}

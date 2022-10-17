0973dfba34fe0b3e197fc8d281232e6088fe8d81
Vytautas87 <vytautaszen1@gmail.com>
2022-10-17 10:17:41 +0300

    skaiciai

--- skaiciai.js
+++ skaiciai.js
@@ -7,11 +7,15 @@ function veiksmai(veiksm, a, b) {
     switch (veiksm) {
         case "sudėtis":
             console.log(a + b);
+        break;
         case "daugyba":
             console.log(a * b);
+        break;
         case "dalyba":
             console.log(a / b);
+        break;
         case "atimtis":
             console.log(a - b);
+        break;
         }
 }
